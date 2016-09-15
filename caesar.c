3ca42045bcf10a2066d29bcb459c1ef004750672
Alex <alexanderchapman@outlook.com>
2016-09-15 22:58:36 +0100

    Updated caesar.c
    Corrected error in argument count checking. It was allowing the user to enter no argument - changes lines 13- 39. Check now rejects no user int and anymore than 1.

--- caesar.c
+++ caesar.c
@@ -7,34 +7,37 @@ int i = 0 ;
 
 int main(int argc, string argv[])
 {
-
-string userText ="";
-
-if (argc < 3) {
-
-printf("Dear user, what text would you like to encode? \n");
-
-userText = GetString();
-
-int argCount = argc ;
-string j = argv[0];
-int k = atoi(argv[1]);
-
-printf ("argc =  %d \n" , argCount );
-printf ("argv[0] =  %s \n" , j );
-printf ("argv[1] =  %d \n" , k );
-printf ("userText = %s \n" , userText);
-return 0;
-
-} else {
     
-    int userArgc = argc - 1;
+    string userText ="";
     
-    printf ("This programme only accepts 1 user command line argument. You input: %i \n", userArgc);
+// checks number of command line arguments  - users can only enter one int which is the rotation key.    
+
+    if (argc < 2 || argc > 2){
+        
+        int userArgc = argc - 1;
+        
+        printf ("This programme only accepts 1 user command line argument. You input: %i \n", userArgc);
+        
+        return 1;
     
-    return 1;
+    } else {
+        
+        printf("Dear user, what text would you like to encode? \n");
     
-}
+        userText = GetString();
+    
+        int argCount = argc ;
+        string j = argv[0];
+        int k = atoi(argv[1]);
+        
+        printf ("argc =  %d \n" , argCount );
+        printf ("argv[0] =  %s \n" , j );
+        printf ("argv[1] =  %d \n" , k );
+        printf ("userText = %s \n" , userText);
+        return 0;
+
+    }
+
 
 
 }
